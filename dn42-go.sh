961dd127f88188149522caa362812053263b1340
EdenJohnson2006 <ayok_ayok@outlook.com>
2022-01-22 23:05:37 +0800

    add missing ";" some fix

--- dn42-go.sh
+++ dn42-go.sh
@@ -21,7 +21,7 @@ apt upgrade -y
 apt install bird2 wireguard babeld -y
 
 # Write things before download
-echo "define OWNAS = ${OWNAS};\ndefine OWNIP = ${OWNIP};\ndefine OWNIPv6 = ${OWNIPv6};\ndefine OWNNET = ${OWNNET};\ndefine OWNNETv6 = ${OWNNETv6};\ndefine OWNNETSET = [${OWNNET}+];\ndefine OWNNETSETv6 = [${OWNNETv6}+]\n\n " > /tmp/bird.conf
+echo "define OWNAS = ${OWNAS};\ndefine OWNIP = ${OWNIP};\ndefine OWNIPv6 = ${OWNIPv6};\ndefine OWNNET = ${OWNNET};\ndefine OWNNETv6 = ${OWNNETv6};\ndefine OWNNETSET = [${OWNNET}+];\ndefine OWNNETSETv6 = [${OWNNETv6}+];\n\n " > /tmp/bird.conf
 
 # Download common things from Internet
 curl https://raw.githubusercontent.com/Eden7Ba23/DN42-Go/main/bird.conf >> /tmp/bird.conf
@@ -35,11 +35,11 @@ echo "net.ipv4.conf.all.rp_filter=0" >> /etc/sysctl.conf
 sysctl -p
 
 # wireguard keygen
-wg genkey | tee privatekey | wg pubkey > /tmp/publickey
+wg genkey | tee /tmp/privatekey | wg pubkey > /tmp/publickey
 mkdir /root/wgkey/peers -p
 mv /tmp/privatekey /root/wgkey/peers
 mv /tmp/publickey /root/wgkey/peers
-wg genkey | tee privatekey | wg pubkey > /tmp/publickey
+wg genkey | tee /tmp/privatekey | wg pubkey > /tmp/publickey
 mkdir /root/wgkey/nodes -p
 mv /tmp/privatekey /root/wgkey/nodes
 mv /tmp/publickey /root/wgkey/nodes
