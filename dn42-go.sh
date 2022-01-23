fa23d9cbb61a6d78d8380dd1f0f2fdd0a887f286
EdenJohnson2006 <ayok_ayok@outlook.com>
2022-01-23 09:07:27 +0800

    fix "masked" error

--- dn42-go.sh
+++ dn42-go.sh
@@ -53,5 +53,6 @@ mkdir /etc/bird/peers
 mkdir /etc/bird/nodes
 curl -sfSLR -o /etc/bird/roa_dn42.conf https://dn42.burble.com/roa/dn42_roa_bird2_4.conf && curl -sfSLR -o /etc/bird/roa_dn42_v6.conf https://dn42.burble.com/roa/dn42_roa_bird2_6.conf && /usr/sbin/birdc configure 1> /dev/null
 (echo "0 */4 * * * curl -sfSLR -o /etc/bird/roa_dn42.conf https://dn42.burble.com/roa/dn42_roa_bird2_4.conf && curl -sfSLR -o /etc/bird/roa_dn42_v6.conf https://dn42.burble.com/roa/dn42_roa_bird2_6.conf && /usr/sbin/birdc configure 1> /dev/null" ; crontab -l) | crontab
+systemctl unmask bird
 systemctl enable bird
 bird
